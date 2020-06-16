754c0ef586ab8a5aa9fa718ebc90db66fa7cf13e
Stanislav Kucher <kucherstanislav@gmail.com>
2020-06-16 20:33:44 +0300

    Initial project's structure was added.

---
+++ src/main/java/com/exhibitionCalendarSpring/ExhibitionCalendarSpringApplication.java
@@ -0,0 +1,13 @@
+package com.exhibitionCalendarSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionCalendarSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionCalendarSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exhibitionCalendarSpring/ExhibitionCalendarSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exhibitionCalendarSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionCalendarSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
